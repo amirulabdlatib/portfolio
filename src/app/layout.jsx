89339c35f8f5d26a3e26faeb22d55a0d4906ea44
Amirul <amirulal73@gmail.com>
2026-01-08 20:47:07 +0800

    Apply next theme

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import { ThemeProvider } from "next-themes";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -18,8 +19,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
     return (
-        <html lang="en">
-            <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+        <html lang="en" suppressHydrationWarning>
+            <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+                <ThemeProvider attribute="class" enableSystem defaultTheme="system">
+                    {children}
+                </ThemeProvider>
+            </body>
         </html>
     );
 }
